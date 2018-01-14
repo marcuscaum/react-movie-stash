39911096e430c68e3bc1200bd1926f2ad2e2cde4
marcuscaum <marcuslucas.jb@gmail.com>
2018-01-14 15:09:18 -0200

    Setup material ui

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { compose } from 'recompose';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
@@ -21,10 +24,20 @@ const store = createStore(
 );
 /* eslint-enable */
 
+const darkTheme = getMuiTheme(darkBaseTheme);
+const muiTheme = getMuiTheme({
+  palette: {
+    ...darkTheme.palette,
+    primary1Color: '#00bcd4',
+  },
+});
+
 export const Routes = () => (
   <Provider store={store}>
     <BrowserRouter>
-      <Route path="/" component={App} />
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <Route path="/" component={App} />
+      </MuiThemeProvider>
     </BrowserRouter>
   </Provider>
 );
--- src/shared/containers/App/index.js
+++ src/shared/containers/App/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
 import Movies from '../../../movies';
 
+injectTapEventPlugin();
+
 export default () => (
-  <Switch>
-    <Route path="/movies" component={Movies} />
-    <Redirect from="/" to="/movies" />
-  </Switch>
+  <div>
+    <Drawer open>
+      <MenuItem>Menu Item</MenuItem>
+      <MenuItem>Menu Item 2</MenuItem>
+    </Drawer>
+    <AppBar
+      title="Title"
+      iconClassNameRight="muidocs-icon-navigation-expand-more"
+    />
+    <Switch>
+      <Route path="/movies" component={Movies} />
+      <Redirect from="/" to="/movies" />
+    </Switch>
+  </div>
 );
